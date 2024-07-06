e95a0fa44f84a45d28641e4e2969ff7af9cbab98
Bhuvnesh verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2024-07-06 18:00:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,46 @@ const UserModel = require("./models/Users");
 const app = express();
 
 // Enable CORS for specific origins
-app.use(cors({
-  origin: ["https://masterbhuvnesh.github.io", "http://localhost:3000"],
-  methods: ["POST", "GET"],
-  credentials: true // Optional, depending on your use case
-}));
-
+app.use(
+  cors({
+    origin: [
+      "https://masterbhuvnesh.github.io",
+      "http://localhost:3000",
+      "http://localhost:3001",
+    ],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Connect to MongoDB
-// mongoose.connect("mongodb+srv://bhuvneshverma:Verma29042005@collage.lkkh7mp.mongodb.net/NotesAPI");
-mongoose.connect("mongodb+srv://bhuvneshverma29042005:Verma29042005$@notes.azp7xvl.mongodb.net/notes");
+mongoose.connect(
+  "mongodb+srv://bhuvneshverma29042005:Verma29042005$@notes.azp7xvl.mongodb.net/notes",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 // GET ALL USER DATA
 app.get("/", (req, res) => {
   UserModel.find({})
     .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+// ADD NEW NAME TO THE SELECTED TYPE
+app.post("/add", (req, res) => {
+  const { type, name } = req.body;
+
+  UserModel.findOneAndUpdate(
+    { type: type },
+    { $push: { names: name } },
+    { new: true }
+  )
+    .then(() => {
+      UserModel.find({})
+        .then((users) => res.json(users))
+        .catch((err) => res.json(err));
+    })
     .catch((err) => res.json(err));
 });
 
