5f6ea5a240e5bd7a06d8b838ac13831ee7e025c1
Bhuvnesh Verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2024-09-17 22:01:10 +0530

    Update Users.js

--- models/Users.js
+++ models/Users.js
@@ -2,9 +2,6 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  id: Number,
-  type: String,
-  names: Array,
 });
-const UserModel = mongoose.model("types", UserSchema);
+const UserModel = mongoose.model("styles", UserSchema);
 module.exports = UserModel;
