ad769012f0b57f4b06714db151df1b8747006234
Bhuvnesh Verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2024-09-19 20:45:56 +0530

    Update Image.js

--- models/Image.js
+++ models/Image.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const ImgSchema = new mongoose.Schema({
+  name: String,
+  pics: String, 
 });
+
 const ImgModel = mongoose.model("img", ImgSchema);
 module.exports = ImgModel;
