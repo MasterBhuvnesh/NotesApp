58b8bb8f763895f7c47ffe037f6e3878a3153527
Bhuvnesh Verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2024-09-24 00:34:03 +0530

    Delete models/Users.js

--- models/Users.js
+++
@@ -1,7 +0,0 @@
-// TRANING THE MODEL
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-});
-const UserModel = mongoose.model("styles", UserSchema);
-module.exports = UserModel;
